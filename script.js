5cc2f1c6c0350e0b4ca11fdaac7d69765a4fc2e1
Gyan prakash Chaurasiya <gpchaurasiya514@gmail.com>
2026-04-15 14:48:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const wrapper = document.querySelector(".wrapper");
+    const loginLink = document.querySelector(".login-link");
+    const registerLink = document.querySelector(".register-link");
+    const btnPopup = document.querySelector(".btnlogin-popup");
+    const iconClose = document.querySelector(".icon-close");
+
+    if(btnPopup){
+        btnPopup.addEventListener("click", () => {
+            wrapper.classList.add("active-popup");
+        });
+    }
+
+    if(iconClose){
+        iconClose.addEventListener("click", () => {
+            wrapper.classList.remove("active-popup");
+        });
+    }
+
+    if(registerLink){
+        registerLink.addEventListener("click", () => {
+            wrapper.classList.add("active");
+        });
+    }
+
+    if(loginLink){
+        loginLink.addEventListener("click", () => {
+            wrapper.classList.remove("active");
+        });
+    }
+
+});
+
+
+
